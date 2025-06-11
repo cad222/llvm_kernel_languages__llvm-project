e1b5fbb8b046a9ef002add88cec348d12c368bd9
Jonas Greifenhain <cadivus@daverkomp.de>
2025-06-11 15:28:18 -0700

    Test1

--- offload/languages/kernel/include/LanguageLaunch.h
+++ offload/languages/kernel/include/LanguageLaunch.h
@@ -14,6 +14,15 @@
 #include <cstddef>
 
 #ifdef __cplusplus
+struct CallConfigurationTy {
+  dim3 GridSize;
+  dim3 BlockSize;
+  size_t SharedMemory;
+  void *Stream;
+};
+
+static thread_local CallConfigurationTy CC = {};
+
 extern "C" {
 #endif
 
@@ -23,11 +32,25 @@ struct LLVMOffloadKernelArgsTy {
   void *_;
 };
 
-unsigned llvmPushCallConfiguration(dim3 __grid_size, dim3 __block_size,
-                                   size_t __shared_memory, void *__stream);
+inline unsigned llvmPushCallConfiguration(dim3 __grid_size, dim3 __block_size,
+                                   size_t __shared_memory, void *__stream) {
+  CallConfigurationTy &Kernel = CC;
+  Kernel.GridSize = __grid_size;
+  Kernel.BlockSize = __block_size;
+  Kernel.SharedMemory = __shared_memory;
+  Kernel.Stream = __stream;
+  return 0;
+}
 
-unsigned llvmPopCallConfiguration(dim3 *__grid_size, dim3 *__block_size,
-                                  size_t *__shared_memory, void *__stream);
+inline unsigned llvmPopCallConfiguration(dim3 *__grid_size, dim3 *__block_size,
+                                  size_t *__shared_memory, void *__stream) {
+  CallConfigurationTy &Kernel = CC;
+  *__grid_size = Kernel.GridSize;
+  *__block_size = Kernel.BlockSize;
+  *__shared_memory = Kernel.SharedMemory;
+  *((void **)__stream) = Kernel.Stream;
+  return 0;
+}
 
 ol_result_t llvmLaunchKernelImpl(const char *KernelID, dim3 GridDim,
                                  dim3 BlockDim, void *KernelArgsPtr,
